55d1460484c3ba4d77ce8e49b63240a2d071fbe9
Thomas Dürr <thomduerr@gmail.com>
2019-03-02 22:28:32 +0100

    init code

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Quote from "./js/components/Quote";
+
+const data = [
+  {
+    text:
+      "Probleme kann man niemals mit der selbsen Denkweise lösen, durch die sie entstanden sind",
+    author: "Albert Einstein"
+  },
+  {
+    text: "Man braucht nichts im Leben fürchten, man muss nur alles verstehen.",
+    author: "Marie Curie"
+  },
+  {
+    text: "Nichts ist so beständig wie der Wandel",
+    author: "Heraklit"
+  }
+];
+
+export default class App extends Component {
+  state = { index: 0 };
+
+  nextQuote = () => {
+    this.setState(state => ({
+      index: (state.index + 1) % data.length
+    }));
+  };
+
+  previousQuote = () => {
+    this.setState(state => ({
+      index: (state.index - 1 + data.length) % data.length
+    }));
+  };
+
+  render() {
+    let { index } = this.state;
+    const quote = data[index];
+    return (
+      <View style={styles.container}>
+        <Text>{quote.text}</Text>
+        <Text>-- {quote.author}</Text>
+        <Quote />
+        <Button title="Nächstes Zitat" onPress={this.nextQuote} />
+        <Button title="Vorheriges Zitat" onPress={this.previousQuote} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: "10%",
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ js/components/Quote.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export default class Quote extends Component {
+  render() {
+    return <Text>Foo</Text>;
+  }
+}
