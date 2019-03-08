db800a3ea92153450daf06f478aa5220b6bb89ef
Thomas Dürr <thomduerr@gmail.com>
2019-03-08 23:45:18 +0100

    center save button

--- js/components/NewQuote.js
+++ js/components/NewQuote.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
-import { Button, Modal } from "react-native";
+import { Button, Modal, StyleSheet, View } from "react-native";
 
 export default class NewQuote extends Component {
   render() {
     const { visible, onSave } = this.props;
     return (
       <Modal visible={visible} onRequestClose={onSave} animationType="slide">
-        <Button title="Speichern" onPress={onSave} />
+        <View style={styles.container}>
+          <Button title="Speichern" onPress={onSave} />
+        </View>
       </Modal>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
