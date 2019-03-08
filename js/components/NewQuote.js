e4e2e5477c67f8d74d06b86be131d7e24047b9a9
Thomas Dürr <thomduerr@gmail.com>
2019-03-09 00:03:39 +0100

    text input styles

--- js/components/NewQuote.js
+++ js/components/NewQuote.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Modal, StyleSheet, View } from "react-native";
+import { Button, Modal, StyleSheet, View, TextInput } from "react-native";
 
 export default class NewQuote extends Component {
   render() {
@@ -7,6 +7,17 @@ export default class NewQuote extends Component {
     return (
       <Modal visible={visible} onRequestClose={onSave} animationType="slide">
         <View style={styles.container}>
+          <TextInput
+            style={[styles.input, { height: 150 }]}
+            multiline={true}
+            placeholder="Inhalt des Zitats"
+            underlineColorAndroid="transparent"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Autor/in des Zitats"
+            underlineColorAndroid="transparent"
+          />
           <Button title="Speichern" onPress={onSave} />
         </View>
       </Modal>
@@ -17,7 +28,18 @@ export default class NewQuote extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginTop: 50
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "deepskyblue",
+    borderRadius: 4,
+    width: "80%",
+    marginBottom: 20,
+    fontSize: 18,
+    padding: 10,
+    height: 50
   }
 });
